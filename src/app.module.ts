313f625068257390b749f2bf289137c5b58779e1
naishiuan <michaelnccu@gmail.com>
2022-06-08 21:39:33 +0800

    MongoDB Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { CoffeesModule } from './coffees.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffees.module.ts
+++ src/coffees.module.ts
@@ -1,10 +1,19 @@
 import { CoffeesService } from './coffees/coffees.service';
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees/coffees.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coffee, CoffeeSchema } from './coffees/entities/coffee.entity';
 
 @Module({
   controllers: [CoffeesController],
   providers: [CoffeesService],
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coffee.name,
+        schema: CoffeeSchema,
+      },
+    ]),
+  ],
 })
 export class CoffeesModule {}
