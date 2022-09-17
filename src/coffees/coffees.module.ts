db93170571551c824ffb5a5b0c707a02df67dfef
Michael Cheng <michaelnccu@gmail.com>
2022-09-17 14:13:45 +0800

    Control NestJS Module Encapsulation

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -9,5 +9,6 @@ import { Flavor } from './entities/flavor.entity';
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
   controllers: [CoffeesController],
   providers: [CoffeesService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
